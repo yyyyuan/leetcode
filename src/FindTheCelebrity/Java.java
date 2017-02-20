ac497b348f856b54ebff747e6f5b7c805d576bfb
yyyyuan <alexyuan357@gmail.com>
2017-02-20 17:59:20 -0500

    Create Java.java

---
+++ src/FindTheCelebrity/Java.java
@@ -0,0 +1,17 @@
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        int candidate = 0;
+        for (int i=0; i<n; i++) {
+            if (knows(candidate, i))
+                candidate = i;
+        }
+        
+        for (int i=0; i<candidate; i++) if (knows(candidate, i)) return -1;
+        for (int i=0; i<n; i++) if (!knows(i, candidate)) return -1;
+        
+        return candidate;
+    }
+}
