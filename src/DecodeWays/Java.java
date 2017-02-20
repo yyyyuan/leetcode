5bfa7974b474250bea846b1cf0fe6241e386a3d8
yyyyuan <alexyuan357@gmail.com>
2017-02-20 16:16:18 -0500

    Create Java.java

---
+++ src/DecodeWays/Java.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        if (n == 0) return 0;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        
+        for (int i=1; i<n; i++) {
+            if (s.charAt(i) != '0') dp[i+1] += dp[i];
+            if ((s.charAt(i-1) == '2' && s.charAt(i) - '0' <= 6) || s.charAt(i-1) == '1') dp[i+1] += dp[i-1];
+        }
+        
+        return dp[n];
+    }
+}
