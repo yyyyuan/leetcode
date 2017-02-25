b8bed1e06eeefc5df68181f4518e7a16078e8427
yyyyuan <alexyuan357@gmail.com>
2017-02-24 19:13:29 -0500

    Update JavaUsingRecursive.java

--- src/ReverseLikedList/JavaUsingRecursive.java
+++ src/ReverseLikedList/JavaUsingRecursive.java
@@ -1,1 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        
+        ListNode start = reverseList(head.next);
+        ListNode temp = start;
+        while (temp.next != null) temp = temp.next;
+        head.next = null;
+        temp.next = head;
+        
+        return start;
+    }
 
+}
