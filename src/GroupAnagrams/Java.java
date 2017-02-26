05835ce0e0c45673cd189cd53d68f9f61a36f15f
yyyyuan <alexyuan357@gmail.com>
2017-02-25 21:10:26 -0500

    Create Java.java

---
+++ src/GroupAnagrams/Java.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<List<String>>();
+        if (strs.length == 0) return result;
+        HashMap<String, List<String>> map = new HashMap<String, List<String>>();
+        Arrays.sort(strs);
+        
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String keyStr = String.valueOf(ca);
+            if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<String>());
+            map.get(keyStr).add(s);
+        }
+        
+        return new ArrayList<List<String>>(map.values());
+    }
+}
