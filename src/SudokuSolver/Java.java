e273041c08d17630ab29b22483e722379b8cf190
yyyyuan <alexyuan357@gmail.com>
2017-06-22 00:49:02 -0400

    Update Java.java
    Update the conditions in "isValid" function (delete some unused conditions) so it will run faster.

--- src/SudokuSolver/Java.java
+++ src/SudokuSolver/Java.java
@@ -26,9 +26,9 @@ public class Solution {
     
     private boolean isValid(char[][] board, int row, int col, char c) {
         for (int i=0; i<9; i++) {
-            if (board[row][i] != '.' && board[row][i] == c) return false; // check row
-            if (board[i][col] != '.' && board[i][col] == c) return false; // check col
-            if (board[3 * (row/3) + i/3][3 * (col/3) + i%3] != '.' && board[3 *(row/3) + i/3][3 * (col/3) + i%3] == c) return false;
+            if (board[row][i] == c) return false; // check row
+            if (board[i][col] == c) return false; // check col
+            if (board[3 *(row/3) + i/3][3 * (col/3) + i%3] == c) return false; // check the cube
         }
         return true;
     }
