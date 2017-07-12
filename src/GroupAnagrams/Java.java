ea0b6169cc212d3296cbf89a91576a3b54aaaec9
yyyyuan <alexyuan357@gmail.com>
2017-07-12 09:43:48 -0400

    Update Java.java

--- src/GroupAnagrams/Java.java
+++ src/GroupAnagrams/Java.java
@@ -3,7 +3,7 @@ public class Solution {
         List<List<String>> result = new ArrayList<List<String>>();
         if (strs.length == 0) return result;
         HashMap<String, List<String>> map = new HashMap<String, List<String>>();
-//        Arrays.sort(strs);
+//        Arrays.sort(strs);    No need for sorting the string array here.
         
         for (String s : strs) {
             char[] ca = s.toCharArray();
