dd8b926ab18e065f8abe9f9a3ea7aaf808f99664
yyyyuan <alexyuan357@gmail.com>
2017-07-14 09:51:00 -0400

    Create NON-DP.java

---
+++ src/MaximumSubarray/NON-DP.java
@@ -0,0 +1,15 @@
+// This problem can also be solved using non-dp method.
+
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int sum = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(sum + nums[i], nums[i]);
+            max = Math.max(max, sum);
+        }
+        
+        return max;
+    }
+}
