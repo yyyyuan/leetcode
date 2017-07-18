2c7008597b3de86929ede7dec222964af6c1cdb2
yyyyuan <alexyuan357@gmail.com>
2017-07-18 17:22:23 -0400

    Update Java.java

--- src/SpiralMatrixII/Java.java
+++ src/SpiralMatrixII/Java.java
@@ -8,16 +8,16 @@ public class Solution {
         int u = 0, d = n - 1, l = 0, r = n - 1;
         while (true) {
             for (int col = l; col <= r; col++) result[u][col] = index++;
-            if (++u > d) break;
+            if (++u > d) break;     // Here it's faster.
             
             for (int row = u; row <= d; row++) result[row][r] = index++;
-            if (r-- <= l) break;
+            if (--r < l) break;
             
             for (int col = r; col >= l; col--) result[d][col] = index++;
-            if (d-- <= u) break;
+            if (--d < u) break;
             
             for (int row = d; row >= u; row--) result[row][l] = index++;
-            if (l++ >= r) break;
+            if (++l > r) break;
         }
         
         return result;
