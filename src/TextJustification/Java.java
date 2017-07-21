def30d10fc04a000e728f62d0a2a838697e0947d
yyyyuan <alexyuan357@gmail.com>
2017-07-21 18:21:14 -0400

    Create Java.java

---
+++ src/TextJustification/Java.java
@@ -0,0 +1,45 @@
+public class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> result = new LinkedList<>();
+        
+        int index = 0;
+        while (index < words.length) {
+            int count = words[index].length();
+            int last = index + 1;
+            while (last < words.length) {
+                if (count + words[last].length() + 1 > maxWidth) break;
+                count += words[last].length() + 1;
+                last++;
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            int diff = last - index - 1;
+            if (last == words.length || diff == 0) {
+                for (int i = index; i < last; i++) {
+                    sb.append(words[i] + " ");
+                }
+                sb.deleteCharAt(sb.length() - 1);
+                for (int i = sb.length(); i < maxWidth; i++) {
+                    sb.append(" ");
+                }
+            }
+            else {
+                int spaces = (maxWidth - count) / diff;
+                int r = (maxWidth - count) % diff;
+                for (int i = index; i < last; i++) {
+                    sb.append(words[i]);
+                    if (i < last - 1) {
+                        for (int j = 0; j <= (spaces + ((i - index) < r ? 1 : 0)); j++) {
+                            sb.append(" ");
+                        }
+                    }
+                }
+            }
+            
+            result.add(sb.toString());
+            index = last;
+        }
+        
+        return result;
+    }
+}
