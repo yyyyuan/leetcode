a31e2697852ef2904058fad0c21e7d9382805694
yyyyuan <alexyuan357@gmail.com>
2017-07-20 22:15:45 -0400

    Create Java.java

---
+++ src/ValidNumber/Java.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public boolean isNumber(String s) {
+        s = s.trim();
+        
+        boolean pointSeen = false;
+        boolean eSeen = false;
+        boolean numberSeen = false;
+        boolean charAfterE = false;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if ('0' <= s.charAt(i) && s.charAt(i) <= '9') {
+                numberSeen = true;
+                charAfterE = false;
+            }
+            else if (s.charAt(i) == '.') {
+                if (eSeen || pointSeen) return false;
+                pointSeen = true;
+            }
+            else if (s.charAt(i) == 'e') {
+                if (eSeen || !numberSeen) return false;
+                charAfterE = true;
+                eSeen = true;
+            }
+            else if (s.charAt(i) == '-' || s.charAt(i) == '+') {
+                if (i != 0 && s.charAt(i - 1) != 'e') return false;
+            }
+            else return false;
+        }
+        
+        return numberSeen && !charAfterE;
+    }
+}
