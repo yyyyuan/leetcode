cbc0947ce49130f18ec6837f0b21890fe49c4994
yyyyuan <alexyuan357@gmail.com>
2017-07-23 10:27:41 -0400

    Create Java.java

---
+++ src/ClimbingSteps/Java.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int climbStairs(int n) {
+        int[] steps = new int[n + 1];
+        steps[0] = 1;
+        
+        for (int i = 0; i <= n; i++) {
+            if (i + 1 <= n) steps[i + 1] += steps[i];
+            if (i + 2 <= n) steps[i + 2] += steps[i];
+        }
+        
+        return steps[n];
+    }
+}
