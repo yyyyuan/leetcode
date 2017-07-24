153db880f647b44abd0800bfea97d2091000a1c4
yyyyuan <alexyuan357@gmail.com>
2017-07-24 18:52:17 -0400

    Update Java.java

--- src/BacktrackProblems/Combinations/Java.java
+++ src/BacktrackProblems/Combinations/Java.java
@@ -7,13 +7,17 @@ public class Solution {
     }
     
     private void backtrack(List<List<Integer>> result, List<Integer> tempList, int start, int n, int k) {
-        if (tempList.size() == k) result.add(new ArrayList<>(tempList));
+        if (tempList.size() == k) result.add(new ArrayList(tempList));
         else {
-            for (int i = start; i <= n; i++) {
-                tempList.add(i);
-                backtrack(result, tempList, i + 1, n, k);
-                tempList.remove(tempList.size() - 1);
-            }
+            int size = tempList.size();
+            if ((k - size) <= (n - start + 1)) {            // Here we could check if there is enough numbers left for combinations, if not, we will skip it. (That's much faster.)
+                 for (int i = start; i <= n; i++) {
+                    tempList.add(i);
+                    backtrack(result, tempList, i + 1, n, k);
+                    tempList.remove(tempList.size() - 1);
+                }    
+            } 
+
         }
     }
 }
