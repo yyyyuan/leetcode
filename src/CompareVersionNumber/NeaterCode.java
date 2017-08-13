78c2ff799d0c0e766f8cd303e673f1a90e8972ca
yyyyuan <alexyuan357@gmail.com>
2017-08-13 11:50:17 -0400

    Update NeaterCode.java

--- src/CompareVersionNumber/NeaterCode.java
+++ src/CompareVersionNumber/NeaterCode.java
@@ -7,10 +7,10 @@ public class Solution {
         int n = Math.max(s1.length, s2.length);
         
         for (int i = 0; i < n; i++) {
-            Integer a = i < s1.length ? (Integer.parseInt(s1[i])) : 0;
-            Integer b = i < s2.length ? (Integer.parseInt(s2[i])) : 0;
-            int compare = a.compareTo(b);
-            if (compare != 0) return compare;
+            int a = i < s1.length ? (Integer.parseInt(s1[i])) : 0;
+            int b = i < s2.length ? (Integer.parseInt(s2[i])) : 0;
+            if (a < b) return -1;
+            if (a > b) return 1;
         }
         return 0;
     }
