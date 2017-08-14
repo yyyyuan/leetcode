1f8a9584b9749053ba1770678be97a9ba8164c98
yyyyuan <alexyuan357@gmail.com>
2017-08-14 14:39:57 -0400

    Create Divide-and-Conquer.java

---
+++ src/Majority/Divide-and-Conquer.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int majorityElement(int[] nums) {
+        return majority(nums, 0, nums.length - 1);
+    }
+    
+    private int majority(int[] nums, int start, int end) {
+        
+        if (start == end) return nums[start];
+                
+        int mid = start + (end - start) / 2;
+        int a = majority(nums, start, mid);
+        int b = majority(nums, mid + 1, end);
+        
+        if (a == b) return a;
+        
+        int countA = 0;
+        int countB = 0;
+        for (int i = start; i <= end; i++) {
+            if (nums[i] == a) countA++;
+            if (nums[i] == b) countB++;
+        }
+        
+        return (countA > countB) ? a : b;
+    }
+}
