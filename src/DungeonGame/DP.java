6ab2ac0502cd9a51d7d0d5e319b2d6b73540024e
yyyyuan <alexyuan357@gmail.com>
2017-08-14 17:44:41 -0400

    Create DP.java

---
+++ src/DungeonGame/DP.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        if (dungeon.length == 0 || dungeon[0].length == 0) return -1;
+        int m = dungeon.length, n = dungeon[0].length;
+        int[][] health = new int[m][n];
+        health[m - 1][n - 1] = Math.max(1 - dungeon[m - 1][n - 1], 1);
+        
+        for (int i = m - 2; i >= 0; i--) {
+            health[i][n - 1] = Math.max(health[i + 1][n - 1] - dungeon[i][n - 1], 1);
+        }
+        
+        for (int j = n - 2; j >= 0; j--) {
+            health[m - 1][j] = Math.max(health[m - 1][j + 1] - dungeon[m - 1][j], 1);
+        }
+        
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                health[i][j] = Math.max(Math.min(health[i + 1][j], health[i][j + 1]) - dungeon[i][j], 1);
+            }
+        }
+        
+        return health[0][0];
+        
+    }
+}
