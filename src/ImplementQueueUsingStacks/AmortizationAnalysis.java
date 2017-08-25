fc1bea4c6ad1f793dbe897f966336c5b75dd39b1
yyyyuan <alexyuan357@gmail.com>
2017-08-25 16:44:33 -0400

    Create AmortizationAnalysis.java

---
+++ src/ImplementQueueUsingStacks/AmortizationAnalysis.java
@@ -0,0 +1,39 @@
+class MyQueue {
+    Stack<Integer> stack;
+    Stack<Integer> temp = new Stack<>();
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stack = new Stack<>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        while (!stack.isEmpty()) temp.push(stack.pop());
+        stack.push(x);
+        while (!temp.isEmpty()) stack.push(temp.pop());
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        return stack.pop();
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        return stack.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
