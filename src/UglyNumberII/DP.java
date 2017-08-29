f802801d1df77626c9a8b410170c28f05a683731
yyyyuan <alexyuan357@gmail.com>
2017-08-29 18:19:24 -0400

    Create DP.java

---
+++ src/UglyNumberII/DP.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+        int[] k = new int[n];
+        int index2 = 0, index3 = 0, index5 = 0;
+        k[0] = 1;
+        for (int i = 1; i < n; i++) {
+            int min = Math.min(k[index2] * 2, Math.min(k[index3] * 3, k[index5] * 5));
+            if (min == k[index2] * 2) index2++;
+            if (min == k[index3] * 3) index3++;
+            if (min == k[index5] * 5) index5++;
+            if (min <= k[i - 1]) i--;
+            else k[i] = min;
+        }
+        return k[n - 1];
+    }
+}
