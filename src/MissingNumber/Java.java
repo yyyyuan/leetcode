7aa05eb0faf07eceeb4599b83b93c0ec0046f76f
yyyyuan <alexyuan357@gmail.com>
2017-08-29 22:15:38 -0400

    Create Java.java

---
+++ src/MissingNumber/Java.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        int n = nums.length;
+        for (int num : nums) sum += num;
+        
+        return n * (n + 1) / 2 - sum;
+    }
+}
