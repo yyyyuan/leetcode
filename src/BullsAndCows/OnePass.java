007a8689985660bac7ba57ed63f21f0b6d7f72d7
yyyyuan <alexyuan357@gmail.com>
2017-09-03 12:06:50 -0400

    Create OnePass.java

---
+++ src/BullsAndCows/OnePass.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String getHint(String secret, String guess) {
+        int bulls = 0;
+        int cows = 0;
+        int[] numbers = new int[10];
+        for (int i = 0; i < secret.length(); i++) {
+            if (secret.charAt(i) == guess.charAt(i)) bulls++;
+            else {
+                // Use one array to store the information, both strings can have effet on this array, that's weave.
+                if (numbers[secret.charAt(i) - '0']++ < 0) cows++;
+                if (numbers[guess.charAt(i) - '0']-- > 0) cows++;
+            }
+        }
+        
+        return bulls + "A" + cows + "B";
+    }
+}
