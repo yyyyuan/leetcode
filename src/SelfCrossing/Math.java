2a708b841807473363b4423932966574a9c18c98
yyyyuan <alexyuan357@gmail.com>
2017-09-28 17:28:08 -0400

    Create Math.java

---
+++ src/SelfCrossing/Math.java
@@ -0,0 +1,19 @@
+// There are 3 cases to analysis, so we dont need to write complex function to solve it.
+class Solution {
+    public boolean isSelfCrossing(int[] x) {
+        int len = x.length;
+        if (len <= 3) return false;
+        
+        for (int i = 3; i < len; i++) {
+            if (x[i] >= x[i - 2] && x[i - 1] <= x[i - 3]) return true;
+            if (i >= 4) {
+                if (x[i - 1] == x[i - 3] && x[i] + x[i - 4] >= x[i - 2]) return true;
+            }
+            if (i >= 5) {
+                if (x[i - 2] >= x[i - 4] && x[i - 3] >= x[i - 1] && x[i] + x[i - 4] >= x[i - 2] && x[i - 1] + x[i - 5] >= x[i - 3]) return true;
+            }
+        }
+        
+        return false;
+    }
+}
