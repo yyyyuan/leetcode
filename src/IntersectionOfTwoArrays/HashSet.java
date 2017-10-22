32dd8deaa50d7c5b3b614eb1751448571c7c9bcb
yyyyuan <alexyuan357@gmail.com>
2017-10-22 17:25:53 -0400

    Create HashSet.java

---
+++ src/IntersectionOfTwoArrays/HashSet.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        
+        for (int n : nums1) {
+            if (!set.contains(n)) set.add(n);
+        }
+        
+        Set<Integer> dump = new HashSet<>();
+        for (int n : nums2) {
+            if (set.contains(n) && !dump.contains(n)) {
+                dump.add(n);
+            }
+        }
+        
+        int[] result = new int[dump.size()];
+        int index = 0;
+        
+        for (Integer n : dump) {
+            result[index++] = n;
+        }
+        
+        return result;
+    }
+}
